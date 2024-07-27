6d7dd5fd3a64520b774d26d0951865999ad21b65
Ronit Parikh <138072436+Ninjorg@users.noreply.github.com>
2024-07-26 19:34:35 -0700

    reverting back to. old chromedriver

--- main.py
+++ main.py
@@ -22,6 +22,23 @@ chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
 chrome_options.add_experimental_option("useAutomationExtension", False)
 
-import undetected_chromedriver as uc
-driver = uc.Chrome(headless=True,use_subprocess=False)
-driver.get('https://nowsecure.nl')
+# Path to the ChromeDriver executable
+chrome_driver_path = "a"  # Replace with the actual path to your ChromeDriver
+
+# Initialize the Service object
+logger.debug("Initializing ChromeDriver service")
+service = Service(executable_path=chrome_driver_path)
+
+# Initialize the WebDriver
+logger.debug("Starting Chrome WebDriver")
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+try:
+    logger.debug("Navigating to the URL")
+    driver.get("https://www.tldrthis.com/#uploadFileSection")
+    logger.debug("URL loaded successfully")
+    # Add your further actions here...
+finally:
+    logger.debug("Closing the browser")
+    driver.wait()
+    driver.quit()
